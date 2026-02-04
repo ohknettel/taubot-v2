6b6e14fc8c268952bcff030201fe24184b37765e
ohknettel <citricthunderous@gmail.com>
2026-02-04 17:08:20 +0200

    docs: update old API endpoint references

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,53 +9,54 @@
 import sys
 from pathlib import Path
 
-sys.path.insert(0, str(Path(__file__).resolve().parents[2])+'/src')
+sys.path.insert(0, str(Path(__file__).resolve().parents[2]) + "/src")
 
-project = 'Taubot V2 Documentation'
-copyright = '2025, Some Stoner'
-author = 'Some Stoner'
+project = "Taubot V2.1 Documentation"
+copyright = "2026, Some Stoner, ohknettel"
+author = "Some Stoner"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.githubpages',
-    'sphinxcontrib.httpdomain',
-    'sphinxext.opengraph'
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.githubpages",
+    "sphinxcontrib.httpdomain",
+    "sphinxext.opengraph"
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
+autodoc_mock_imports = ["sqlalchemy"]
 exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'pydata_sphinx_theme'
-html_static_path = ['_static']
+html_theme = "pydata_sphinx_theme"
+html_static_path = ["_static"]
 html_show_sourcelink = False
 html_theme_options = {
-  'collapse_navigation': True,
-  'logo': {
-    'text': 'Taubot V2 Documentation',
+  "collapse_navigation": True,
+  "logo": {
+    "text": "Taubot V2.1 Documentation",
    }
 }
 html_sidebars = {
-  '**': []
+  "**": []
 }
-html_favicon = '_static/favicon.png'
-html_logo = '_static/logo.png'
+html_favicon = "_static/favicon.png"
+html_logo = "_static/logo.png"
 
 # -- Options for Oembed ------------------------------------------------------
 # https://sphinxext-opengraph.readthedocs.io/en/latest/#options
 
-ogp_site_url = "https://docs.tau.work.gd"
+ogp_site_url = "https://docs.taubot.qzz.io"
 ogp_image = "_static/logo.png"
 ogp_custom_meta_tags = [
-    '<meta property="og:ignore_canonical" content="true" />',
-    '<meta name="theme-color" content="#e8cc4c" />'
+    "<meta property=\"og:ignore_canonical\" content=\"true\" />",
+    "<meta name=\"theme-color\" content=\"#e8cc4c\" />"
 ]
 ogp_enable_meta_description = True
